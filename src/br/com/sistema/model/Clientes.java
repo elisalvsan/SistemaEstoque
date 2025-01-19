41bcd61fdf121559d978e2450cb3b2ede1314266
Elisa Andrade <elisalvsan@gmail.com>
2025-01-19 10:12:46 -0200

    Criando a classe Clientes no package Model

---
+++ src/br/com/sistema/model/Clientes.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sistema.model;
+
+/**
+ *
+ * @author Elisa
+ */
+public class Clientes {
+    
+    private int Id;
+    private String Nome;
+    private String Rg;
+    private String Cpf;
+    private String Email;
+    private String Telefone;
+    private String Celular;
+    private String Cep;
+    private String Endereco;
+    private int Nummero;
+    private String Complemento;
+    private String Bairro;
+    private String Cidade;
+    private String Estado;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getRg() {
+        return Rg;
+    }
+
+    public void setRg(String Rg) {
+        this.Rg = Rg;
+    }
+
+    public String getCpf() {
+        return Cpf;
+    }
+
+    public void setCpf(String Cpf) {
+        this.Cpf = Cpf;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+
+    public String getCelular() {
+        return Celular;
+    }
+
+    public void setCelular(String Celular) {
+        this.Celular = Celular;
+    }
+
+    public String getCep() {
+        return Cep;
+    }
+
+    public void setCep(String Cep) {
+        this.Cep = Cep;
+    }
+
+    public String getEndereco() {
+        return Endereco;
+    }
+
+    public void setEndereco(String Endereco) {
+        this.Endereco = Endereco;
+    }
+
+    public int getNummero() {
+        return Nummero;
+    }
+
+    public void setNummero(int Nummero) {
+        this.Nummero = Nummero;
+    }
+
+    public String getComplemento() {
+        return Complemento;
+    }
+
+    public void setComplemento(String Complemento) {
+        this.Complemento = Complemento;
+    }
+
+    public String getBairro() {
+        return Bairro;
+    }
+
+    public void setBairro(String Bairro) {
+        this.Bairro = Bairro;
+    }
+
+    public String getCidade() {
+        return Cidade;
+    }
+
+    public void setCidade(String Cidade) {
+        this.Cidade = Cidade;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String Estado) {
+        this.Estado = Estado;
+    }
+    
+    
+    
+    
+}
