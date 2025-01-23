01b7d6d7ee564dc40af93c18533f9b203567c3a7
Elisa Andrade <elisalvsan@gmail.com>
2025-01-23 12:30:39 -0300

    Criando método para listar clientes na tabela

--- src/br/com/sistema/dao/ClientesDAO.java
+++ src/br/com/sistema/dao/ClientesDAO.java
@@ -7,10 +7,13 @@ package br.com.sistema.dao;
 
 import br.com.sistema.jdbc.ConexaoBanco;
 import br.com.sistema.model.Clientes;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -18,13 +21,13 @@ import javax.swing.JOptionPane;
  * @author Elisa
  */
 public class ClientesDAO {
-
+    
     private Connection conn;
-
+    
     public ClientesDAO() {
         this.conn = new ConexaoBanco().pegarConexao();
     }
-
+    
     public void Salvar(Clientes obj) {
         try {
             //Criacão do SQL
@@ -54,14 +57,14 @@ public class ClientesDAO {
             JOptionPane.showMessageDialog(null, "Erro ao salvar o cliente" + erro);
         }
     }
-
+    
     public Clientes BuscarCliente(String nome) {
         try {
             String sql = "select * from tb_clientes where nome = ?";
             PreparedStatement stmt = conn.prepareStatement(sql);
             stmt.setString(1, nome);
             ResultSet rs = stmt.executeQuery();
-
+            
             Clientes obj = new Clientes();
             if (rs.next()) {
                 obj.setId(rs.getInt("id"));
@@ -85,5 +88,38 @@ public class ClientesDAO {
         }
         return null;
     }
-
+    
+    public List<Clientes> listar() {
+        List<Clientes> lista = new ArrayList<>();
+        try {
+            String sql = "select * from tb_clientes";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            
+            while (rs.next()) {                
+                Clientes obj = new Clientes();
+                obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setRg(rs.getString("rg"));
+                obj.setCpf(rs.getString("cpf"));
+                obj.setEmail(rs.getString("email"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setEstado(rs.getString("estado"));
+                lista.add(obj);
+            }
+            return lista;
+            
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao criar a lista" + erro);
+        }
+        return null;
+    }
+    
 }
